ee4c783abebe8fc2f4b125be8eefb79b6d999270
Ismail-Lafhiel <odashimidorida@gmail.com>
2024-10-06 22:06:04 +0100

    edited confirm email controller

--- src/api/v1/controllers/auth/confirmEmailController.js
+++ src/api/v1/controllers/auth/confirmEmailController.js
@@ -2,21 +2,20 @@ const { confirmEmail } = require("../../helpers/authHelper");
 const User = require("../../models/User");
 
 const emailConfirmation = async (req, res) => {
-  const { token } = req.query;
+  const { token } = req.body;
+
   if (!token) {
     return res.status(400).json({ message: "Token is required" });
   }
 
+  // Calling the confirmEmail helper with the token
   const result = await confirmEmail(token, User);
 
   if (!result.success) {
     return res.status(400).json({ message: result.message });
   }
 
-  // Redirect to login page with success message
-  return res.redirect(
-    `${process.env.FRONTEND_URL}/login?message=Email successfully verified`
-  );
+  return res.status(200).json({ message: result.message });
 };
 
 module.exports = {
